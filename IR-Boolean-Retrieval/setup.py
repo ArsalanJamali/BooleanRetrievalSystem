26cdbbfa09a7fbf6253a2c83b45c18f9bf7cc434
ArsalanJamali <jamaliarsalan@yahoo.com>
2021-03-30 01:19:56 +0500

    boolean queries were treated

--- IR-Boolean-Retrieval/setup.py
+++ IR-Boolean-Retrieval/setup.py
@@ -6,16 +6,20 @@ from nltk.stem import PorterStemmer
 
 ps=PorterStemmer()
 DATASET_DIR=os.path.join(os.getcwd(),'dataset/ShortStories/')
+TOTAL_DOCS=None
 
 
 class BooleanRetrievalSystem:
 
     def __init__(self):
         global punctuation
-        punctuation+='“”’‘—'
-        self.vocabulary=dict()
+        punctuation+='“”’‘—'  #inorder to deal with punctuations of different unicode
+        self.vocabulary=dict()  #for positional index
+        self.inverted_index=dict()  #for inverted index
         self.stop_word=["a", "is", "the", "of", "all", "and", "to", "can", "be", "as", "once"
                         , "for", "at", "am", "are", "has", "have", "had", "up", "his", "her", "in", "on", "no", "we", "do"]                
+
+
 
     def pre_process(self,document):
         document=document.lower()  #lowers the text
@@ -42,17 +46,20 @@ class BooleanRetrievalSystem:
 
         for word in word_list:
             if word not in self.vocabulary:
-                self.vocabulary[word]=dict()        
+                self.vocabulary[word]=dict()
+                self.inverted_index[word]=list()
+            self.inverted_index[word].append(docId)
         self.calculate_all_occurance(stem_list,docId)        
         
     def process_txt(self):
+        global TOTAL_DOCS
         for txt_file in os.listdir(DATASET_DIR):
             if txt_file.endswith('.txt'):
                 doc_id=int(txt_file.split('.')[0])
                 f=open(os.path.join(DATASET_DIR,txt_file),'r',encoding='utf-8')
                 word_list,stem_list=self.pre_process(f.read())
                 self.create_vocab(word_list,stem_list,doc_id)
-        #print(self.vocabulary)    
+        TOTAL_DOCS=len(os.listdir(DATASET_DIR))
 
     def intersect_list(self,list_1,list_2):
         intersect_list=list()
@@ -69,13 +76,13 @@ class BooleanRetrievalSystem:
 
         return intersect_list
 
-    def binary_search(self,search_list,num_to_search):
+    def binary_search(self,search_list,position_1,position_2):
         l,r=(0,len(search_list)-1)
         while l<=r:
             m=l +(r-l)//2
-            if search_list[m]==num_to_search:
+            if search_list[m]<=position_1 and search_list[m]>=position_2 :
                 return True
-            elif search_list[m]<num_to_search:
+            elif search_list[m]<position_2:
                 l=m+1
             else:
                 r=m-1
@@ -105,26 +112,103 @@ class BooleanRetrievalSystem:
                 intersect_list2=temp_list
 
             for position in intersect_list1:
-                if self.binary_search(intersect_list2,position+(int(proximity_ratio)+1)):
+                if self.binary_search(intersect_list2,position+(int(proximity_ratio)+1),position+1):
                     proximity_query_final_list.append(docId)
                     break
 
-        print(proximity_query_final_list)        
+        print(proximity_query_final_list)
+        return proximity_query_final_list
 
+
+    def not_of_list(self,list_1):
+        list_2=[ num for num in range(1,TOTAL_DOCS+1) ]
+
+        for num in list_1:
+            list_2.remove(num)
+
+        return list_2    
+
+    def process_boolean_query(self,user_query):
+        op=None  #for storing operator
+        values=list()  #for storing each word in query 
+
+        index=0
+        not_count=0
+        user_query=user_query.lower().split()
+
+        while index<len(user_query):
+            if user_query[index]=='not':
+                while index<len(user_query) and user_query[index]=='not':
+                    index+=1
+                    not_count+=1
+
+                if not_count%2!=0:
+                    if index!=len(user_query):
+                        word=ps.stem(user_query[index])
+                        value=None
+                        try:
+                            value=self.not_of_list(self.inverted_index[word])
+                        except:
+                            value=self.not_of_list(list())
+                        values.append(value)
+                    else:
+                        return []
+
+                else:
+                    if index!=len(user_query):
+                        word=ps.stem(user_query[index])
+                        value=None
+                        try:
+                            value=self.inverted_index[word]
+                        except:
+                            value=list()
+                        values.append(value)
+                    else:
+                        return []    
+                
+                not_count=0
+
+            elif user_query[index]=='and' or user_query[index]=='or':
+                op=user_query[index]
+            else:
+                word=ps.stem(user_query[index])
+                value=None
+                try:
+                    value=self.inverted_index[word]
+                except:
+                    value=list()
+                values.append(value)
+
+            if len(values)>1:
+                if op=='and':
+                    list_1=values.pop()
+                    list_2=values.pop()
+                    list_1.sort()
+                    list_2.sort()
+                    values.append(self.intersect_list(list_1,list_2))
+                elif op=='or':
+                    list_1=values.pop()
+                    list_2=values.pop()
+                    values.append(list(set(list_1) | set(list_2) ))
+                op=None    
+
+            index+=1
+
+
+        return values.pop()
+
+            
 
 if __name__=='__main__':
     model=BooleanRetrievalSystem()
     model.process_txt()
-    user_query=input('Enter your query: ')
-    if '/' in user_query:
-        model.process_proximity_query(user_query)
-
-        
-    
-
-
-
-
-        
-
-
+    flag=True
+    while flag:
+        user_query=input('Enter your query: ')
+        if user_query=='-1':
+            flag=False
+            continue
+        if '/' in user_query:
+            model.process_proximity_query(user_query)
+        else:
+            print(model.process_boolean_query(user_query))
